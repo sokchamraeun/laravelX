d4127fcfed9bd1134f146b498ab4fd7c01b29d9a
visal <sokchamraeunid@gmail.com>
2026-07-01 23:02:09 +0700

    update cose.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,8 +3,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://laravel-n7n7c5nkm-sokchamraeuns-projects.vercel.app'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins_patterns' => ['/^https:\/\/.*\.vercel\.app$/'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
